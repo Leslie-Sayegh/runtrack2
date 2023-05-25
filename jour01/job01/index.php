46416e4cf88183e65ac34e4c544208f3a9ecbc5b
unknown <leslie.sayegh@laplateforme.io>
2023-05-25 16:37:09 +0200

    job01_variable

--- jour01/job01/index.php
+++ jour01/job01/index.php
@@ -1,11 +1,21 @@
 <?php
-$str = "LaPlateforme";
-?>
-
-<?php
+$str = "La Plateforme";
 $str2 = "Vive";
-?>
+$str3 = "!";
+echo $str." ". $str2." ". $str3 . "<br>" ;
+ 
+$val = 6; 
+echo $val . "<br>";
+$val += 4;
+echo $val. "<br>" ;
 
-<?php
-$str2 = "!";
+$myBool = true;
+echo $myBool. "<br>";
+$myBool = false;
+echo $myBool. "<br>";
+
+$myBool = true;
+echo  var_export($myBool, true) . "<br>";
+$myBool = false;
+echo  var_export($myBool, true) . "<br>";
 ?>
