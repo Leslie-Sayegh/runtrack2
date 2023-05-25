dc846c6ec45b2c2bee087a038d6bf2d012d560e2
unknown <leslie.sayegh@laplateforme.io>
2023-05-25 22:38:24 +0200

    job02

---
+++ jour01/job02/index.php
@@ -0,0 +1,32 @@
+<?php
+$phrase = "Hello La Plateforme !";
+echo $phrase;
+
+$message = "Hello La Plateforme !";
+echo $message;
+
+
+$texte = "Hello La Plateforme !";
+echo $texte;
+
+$myVariable = "Hello La Plateforme !";
+echo $myVariable;
+
+$greeting = "Hello La Plateforme !";
+echo $greeting;
+
+$message = "Hello La Plateforme !";
+print $message;
+
+?>
+
+
+<?php
+$message = "Hello La Plateforme !";
+?>
+<?= $message ?>
+
+<?php
+$message = "Hello La Plateforme !";
+printf("%s", $message);
+?>
