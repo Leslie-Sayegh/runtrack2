1b7365a06f16c6be562422eeb20d01436c769d26
unknown <leslie.sayegh@laplateforme.io>
2023-05-26 20:38:58 +0200

    jour04_

--- jour04/job02/index.php
+++ jour04/job02/index.php
@@ -1,1 +1,32 @@
- 
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Tableau HTML</title>
+</head>
+<body>
+    <table border="1">
+        <tr>
+            <th>Arguments</th>
+            <th>Valeurs associées</th>
+        </tr>
+        <tr>
+            <td>Prénom</td>
+            <td><?php echo $_GET['prenom']; ?></td>
+        </tr>
+        <tr>
+            <td>Nom</td>
+            <td><?php echo $_GET['nom']; ?></td>
+        </tr>
+    </table>
+
+    <form action="" method="GET">
+        <label for="prenom">Prénom:</label>
+        <input type="text" name="prenom" id="prenom" required>
+
+        <label for="nom">Nom:</label>
+        <input type="text" name="nom" id="nom" required>
+
+        <input type="submit" value="Envoyer">
+    </form>
+</body>
+</html>
