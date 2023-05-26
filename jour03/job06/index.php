c4adf169fa631c2294c76699c36897ea74c19060
unknown <leslie.sayegh@laplateforme.io>
2023-05-26 14:42:13 +0200

    jour03_;

--- jour03/job06/index.php
+++ jour03/job06/index.php
@@ -1,18 +1,36 @@
 <?php
 $str = "Les choses que l'on possède finissent par nous posséder.";
+echo $str;
 
+$reversedStr = '';
+$length = strlen($str); // Utilisation de mb_strlen pour prendre en compte les caractères spéciaux
 
+for ($i = $length - 1; $i >= 0; $i--) {
+    if ($str[$i] === ' ') {
+        // Ajouter l'espace 
+        $reversedStr .= ' ';
+    } else {
+        // Ajouter le caractère en échappant les caractères spéciaux
+        $reversedStr .= htmlspecialchars($str[$i]);
+    }
+}
+echo $reversedStr;
 
-
-
-
-
-
-
-
-
-
-
-
-
+    var_dump($str);
+    var_dump($reversedStr);
+    var_dump($length);
+   
 ?>
+
+
+
+
+
+
+
+
+
+
+
+
+
