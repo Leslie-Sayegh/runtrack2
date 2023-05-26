138ac411e1f7f605d72dd196141c722e360cede5
unknown <leslie.sayegh@laplateforme.io>
2023-05-26 13:32:57 +0200

    jour03_dictionnaire

--- jour03/job05/index.php
+++ jour03/job05/index.php
@@ -1,11 +1,40 @@
 <?php
-$str ="On n’est pas le meilleur quand on le croit mais quand on le sait";
-echo $str;
+$str = "On n'est pas le meilleur quand on le croit mais quand on le sait";
 
-$array[
-    "key"  => "value",
-    "key2" => "value2",
-    "key3" => "value3",
-   
-];
+$dic = array(
+    "consonnes" => 0,
+    "voyelles" => 0
+);
+
+// Parcours de la chaîne de caractères
+for ($i = 0; $i < strlen($str); $i++) {
+    $char = strtolower($str[$i]); // Conversion en minuscules pour la comparaison
+
+// Vérification si le caractère est une lettre
+    if (preg_match("/[a-z]/i", $char)) {
+        // Vérification si le caractère est une voyelle
+        if (in_array($char, array("a", "e", "i", "o", "u", "y"))) {
+            $dic["voyelles"]++;
+        } else {
+            $dic["consonnes"]++;
+        }
+    }
+    var_dump($str);
+}
+
 ?>
+
+<table>
+  <thead>
+    <tr>
+      <th style="border: 1px solid black; padding: 5px;">Voyelles</th>
+      <th style="border: 1px solid black; padding: 5px;">Consonnes</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td style="border: 1px solid black; padding: 5px;"><?php echo $dic["voyelles"]; ?></td>
+      <td style="border: 1px solid black; padding: 5px;"><?php echo $dic["consonnes"]; ?></td>
+    </tr>
+  </tbody>
+</table>
