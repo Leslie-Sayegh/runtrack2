32641145b0f66e8b0afd4a208c8032ac5b493669
unknown <leslie.sayegh@laplateforme.io>
2023-05-28 03:15:15 +0200

    jour04_pair-impair

--- jour04/job06/index.php
+++ jour04/job06/index.php
@@ -1,1 +1,44 @@
  
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Formulaire PHP</title>
+</head>
+<body>
+
+<form action="index.php" method="get">
+    <label for="nombre">Nombre:</label>
+    <input type="text" id="nombre" name="nombre" required>
+    <input type="submit" value="Valider">
+</form>
+
+</body>
+</html>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Résultat</title>
+</head>
+<body>
+
+<?php
+if (isset($_GET['nombre'])) {
+    $nombre = $_GET['nombre'];
+    if (is_numeric($nombre)) {
+        if ($nombre % 2 == 0) {
+            echo "Nombre pair";
+        } else {
+            echo "Nombre impair";
+        }
+    } else {
+        echo "Veuillez entrer un nombre valide.";
+    }
+} else {
+    echo "Veuillez entrer un nombre.";
+}
+?>
+
+</body>
+</html>
